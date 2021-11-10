b9d9cc8444ec9d26191c041393134b8ff39cc843
Mim <fatemamim28@gmail.com>
2021-11-10 15:38:30 +0600

    order details

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         color2: "#FEB238",
         color3: "#661109",
         color4: "#475993",
+        color5: "rgba(102,17,9,0.6)",
         overlay: "#ffffff6b",
         "overlay-dark": "#00000052",
       },
