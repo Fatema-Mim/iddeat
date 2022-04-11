af8f3285506ec4431e62f5597d95a552eb0c0156
Mim <fatemamim28@gmail.com>
2022-04-11 14:49:54 +0600

    reservation part add

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,17 @@ module.exports = {
         color5: "rgba(102,17,9,0.6)",
         color6: "#28A745",
         color7: "#d92662",
+        color8: "#A78963",
         overlay: "#ffffff6b",
         "overlay-dark": "#00000052",
       },
       backgroundImage: {
         'terms': "url('/public/img/terms_bg.gif')",
         'footer': "url('/public/img/footer.jpg')",
-      }
+      },
+      fontFamily: {
+      'openSans': ['Open Sans','sans-serif' ]
+    }
     },
   },
   variants: {
